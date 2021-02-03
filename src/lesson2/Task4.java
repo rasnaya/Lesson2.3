f8d03ecfc94c543d2e4c7a8c4a856cf410842dcb
rasnaya <rlwork656@gmail.com>
2021-02-03 14:32:38 +0300

    add lesson2

---
+++ src/lesson2/Task4.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int abcd = 9876;
+        int number1 = 9;
+        int number2 = 8;
+        int number3 = 7;
+        int number4 = 6;
+        int result = number4 * 1000 + number3 * 100 + number2 *
+                10 + number1 * 1;
+        System.out.println("Число" + number4 + "" + number3 + "" + number2
+                + "" + number1);
+    }
+}
---
+++ src/lesson2/Task6.java
@@ -0,0 +1,19 @@
+package lesson2;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int abcd = 3453;
+
+        if (abcd % 2 ==0) { System.out.println("четное");
+        } else {
+            System.out.println("не четное");
+        }
+if (abcd > 99 && abcd <= 999) {
+    System.out.println("трехзначное");
+} else { System.out.println("не трехзначное");
+}
+    }
+
+
+
+}
